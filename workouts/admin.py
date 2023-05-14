7a254570e39983770270ef7c51851d4878c28463
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-05-14 12:16:45 +0100

    Made migrations and migrated the database

--- workouts/admin.py
+++ workouts/admin.py
@@ -4,10 +4,10 @@ from . import models
 
 # Register your models here.
 class WorkoutsAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title','created',)
 
 class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ('exercise','weight','reps', 'sets', 'created', 'workouts')
+    list_display = ('exercise','weight','reps', 'sets','workouts',)
 
 admin.site.register(models.Workouts, WorkoutsAdmin)
 admin.site.register(models.Exercise, ExerciseAdmin)
--- workouts/models.py
+++ workouts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Workouts(models.Model):
     title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -16,7 +17,6 @@ class Exercise(models.Model):
     weight = models.DecimalField(max_digits=6, decimal_places=2)
     reps = models.IntegerField()
     sets = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
     workouts = models.ForeignKey(Workouts, on_delete=models.CASCADE)
 
     def __str__(self):
